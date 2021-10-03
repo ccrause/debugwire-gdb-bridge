239905e0749482ea7879cd269cbf4c9d43cea58f
ccrause <christo.crause@gmail.com>
2021-10-03 17:12:35 +0200

    Improved handling of Intel hex format.

--- binutils.pas
+++ binutils.pas
@@ -5,13 +5,28 @@ interface
 uses
   Classes, SysUtils, debugwire;
 
+{ General layout of a line of information in Intel hex file:
+  :BBAAAATT[DDDDDDDD]CC
+
+  where
+  : is start of line marker
+  BB is number of data bytes on line
+  AAAA is address in bytes
+  TT is data type
+  DD is data bytes, number depends on BB value
+  CC is checksum (2s-complement of number of bytes+address+data)
+}
+
 type
-  TBinRecType = (brData=0, brEOF, brExtSegAddr, brStartSegAddr, brExtLinearAddr, brStartLinearAddr);
+  TBinRecType = (brData=0,             // Record contains data and a 16-bit starting address
+                 brEOF,                // Indicate end of file, byte count 0 and address typically 0
+                 brExtSegAddr,         // Extended segment address. Data field contains 16-bit segment address (big endian).  Multiply this with 16 and add to subsequent record addresses
+                 brStartSegAddr,       // For x86 this record specifies the CS:IP registers (4 bytes data)
+                 brExtLinearAddr,      // 2 data bytes in big endian which form upper 16 bits of extended address for subsequent 00 records.
+                 brStartLinearAddr);   // 32 bit address, loaded into EIP register for 386+ CPUs
 
   TBinRecord = record
-    count: word;   // number of bytes in this record
-    address: word; // address of first byte
-    recordType: TBinRecType;
+    address: dword; // address of first byte
     data: TBytes;
   end;
 
@@ -35,11 +50,17 @@ var
   line, s: string;
   i, j, k: integer;
   data, checksum: byte;
+  typ: TBinRecType;
+  numbytes: word;
+  address, addressOffset: dword;
+  gotEOF: boolean;
 begin
   SetLength(Result, 0);
   i := 0;
+  addressOffset := 0;
+  gotEOF := false;
 
-  while SL.Count > 0 do
+  while (SL.Count > 0) and not(gotEOF) do
   begin
     line := SL[0];
     SL.Delete(0);
@@ -50,58 +71,120 @@ begin
       WriteLn('Invalid start, expected '':'' but got ', line[1])
     else
     begin
-      SetLength(Result, i+1);
-      // Read byte count
+      // Read byte count, 1 byte
       s := '$' + copy(line, 2, 2);
-      Result[i].count := StrToInt(s);
-      checksum := Result[i].count;
+      numbytes := StrToInt(s);
+      checksum := numbytes;
 
-      // address, 2 bytes, 4 char
+      // Address, 2 bytes
       s := '$' + copy(line, 4, 2);
-      Result[i].address := StrToInt(s);
-      checksum := checksum + Result[i].address;
-      Result[i].address := Result[i].address shl 8;
+      data := StrToInt(s);
+      address := data shl 8;
+      checksum := byte(checksum + data);
       s := '$' + copy(line, 6, 2);
       data := StrToInt(s);
-      Result[i].address := Result[i].address + data;
-      {$PUSH}
-      {$R-}
-      checksum := checksum + data;
-      {$POP}
+      address := address or data;
+      checksum := byte(checksum + data);
 
-      // Record type
+      // Record type, 1 byte
       s := '$' + copy(line, 8, 2);
-      Result[i].recordType := TBinRecType(StrToInt(s));
-      {$PUSH}
-      {$R-}
-      checksum := checksum + ord(Result[i].recordType);
-      {$POP}
+      typ := TBinRecType(StrToInt(s));
 
-      // Data
-      j := 0;
-      SetLength(Result[i].data, Result[i].count);
-      while j < Result[i].count do
-      begin
-        s := '$' + copy(line, 10+2*j, 2);
-        Result[i].data[j] := StrToInt(s);
-        {$PUSH}
-        {$R-}
-        checksum := checksum + Result[i].data[j];
-        {$POP}
-        inc(j);
+      case typ of
+        brData:
+          begin
+            SetLength(Result, i+1);
+            //Result[i].count := numbytes;
+            Result[i].address := addressOffset + address;
+            //Result[i].recordType := typ;    // TODO: remove this, return only data indexed by linear address
+            // Data
+            j := 0;
+            SetLength(Result[i].data, numbytes);
+            while j < numbytes do
+            begin
+              s := '$' + copy(line, 10+2*j, 2);
+              Result[i].data[j] := StrToInt(s);
+              checksum := byte(checksum + Result[i].data[j]);
+              inc(j);
+            end;
+            inc(i);
+          end;
+
+        brEOF: gotEOF := true;
+
+        brExtSegAddr:
+          begin
+            // Data, should be 2 bytes
+            if numbytes <> 2 then
+              writeln('Unexpected number of bytes for brExtLinearAddr');
+            j := 0;
+            addressOffset := 0;
+            while j < numbytes do
+            begin
+              s := '$' + copy(line, 10+2*j, 2);
+              data := StrToInt(s);
+              addressOffset := (addressOffset shl 8) + data;
+              checksum := byte(checksum + data);
+              inc(j);
+            end;
+            addressOffset := addressOffset shl 4;
+          end;
+
+        brStartSegAddr:
+          begin
+            // CS:IP register pair - ignore for now
+            // Data
+            j := 0;
+            while j < numbytes do
+            begin
+              s := '$' + copy(line, 10+2*j, 2);
+              data := StrToInt(s);
+              checksum := byte(checksum + data);
+              inc(j);
+            end;
+          end;
+
+        brExtLinearAddr:
+          begin
+            // Data, should be just 2 bytes
+            if numbytes <> 2 then
+              writeln('Unexpected number of bytes for brExtLinearAddr');
+            j := 0;
+            addressOffset := 0;
+            while j < numbytes do
+            begin
+              s := '$' + copy(line, 10+2*j, 2);
+              data := StrToInt(s);
+              addressOffset := (addressOffset shl 8) + data;
+              checksum := byte(checksum + data);
+              inc(j);
+            end;
+            addressOffset := addressOffset shl 16;
+          end;
+
+        brStartLinearAddr:
+          begin
+            // Start of linear address for 386+ CPUs. Ignore for now.
+            // Data
+            j := 0;
+            while j < numbytes do
+            begin
+              s := '$' + copy(line, 10+2*j, 2);
+              data := StrToInt(s);
+              checksum := byte(checksum + data);
+              inc(j);
+            end;
+          end;
+      else
+        writeln('Unexpected HEX record type');
       end;
 
       // checksum
-      s := '$' + copy(line, 10+2*Result[i].count, 2);
+      s := '$' + copy(line, 10+2*numbytes, 2);
       data := StrToInt(s);
-      {$PUSH}
-      {$R-}
-      checksum := checksum + data;
-      {$POP}
-      if checksum <> 0 then
+      checksum := byte(checksum + data);
+      if (checksum <> 0) and (typ <> brEOF) then
         WriteLn('Checksum error in hex line');
-
-      inc(i);
     end;
   end;
 
@@ -110,11 +193,9 @@ begin
   j := 1;
   while (j < length(Result) - 1) do
   begin
-    if (Result[j].recordType = Result[i].recordType) and
-       (Result[i].address + Result[i].count = Result[j].address) then
+    if (Result[i].address + length(Result[i].data) = Result[j].address) then
     begin
       Result[i].data := ConCatArray(Result[i].data, Result[j].data);
-      Result[i].count := Result[i].count + Result[j].count;
 
       // Naive compaction, could probably just let 2nd index increment and truncate at end
       for k := j to length(Result)-2 do
@@ -133,9 +214,10 @@ end;
 
 procedure programHexFile(const fileName: string; dw: TDebugWire);
 var
-  i, j: integer;
+  i: integer;
   SL: TStringList;
   binArray: TBinArray;
+  memsection: word;
 begin
   if FileExists(fileName) then
   begin
@@ -145,12 +227,13 @@ begin
 
     for i := 0 to length(binArray)-1 do
     begin
-      case binArray[i].recordType of
-        brData : DW.WriteFlash(binArray[i].address, binArray[i].data);
-        brEOF  : WriteLn('End of Hex data');
+      memsection := hi(binArray[i].address);
+      if memsection < $80 then
+        DW.WriteFlash(binArray[i].address, binArray[i].data)
+      else if memsection = $81 then
+        writeln('EEPROM support not implemented yet')// DW.WriteEEPROM()
       else
-        WriteLn('Unexpected record type for hex record', binArray[i].recordType);
-      end;
+        writeln('Unrecognized virtual memory prefix: $', HexStr(memsection, 4));
     end;
   end;
 end;
